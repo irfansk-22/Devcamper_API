792f75e8d31d1acb514d7bb9ec91263808f736e3
Irfan Shaikh <irfansk.se@gmail.com>
2022-01-12 13:44:49 +0530

    Error handling & async handler

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -26,6 +27,9 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+// Error Handling Middleware: To send error message in json format
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
@@ -34,7 +38,7 @@ const server = app.listen(PORT, () => {
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Nahi hua Error: ${err.message}`.red);
+  console.log(`Nahi hua, Error: ${err.message}`.red);
 
   // Close server & exit process
   server.close(() => process.exit(1));
